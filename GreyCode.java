7319f80eb4aabce3c84a96a8a24497702c717789
Animesh Gupta <animesh.gupta@stonybrook.edu>
2015-09-28 18:47:03 -0400

    Working solution posted...

---
+++ GreyCode.java
@@ -0,0 +1,36 @@
+// Source : https://leetcode.com/problems/gray-code/
+// Author : Animesh Gupta
+// Date   : 2015-09-27
+
+/************************************************************************************************** 
+
+The gray code is a binary numeral system where two successive values differ in only one bit.
+
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+
+For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+Note:
+For a given n, a gray code sequence is not uniquely defined.
+
+For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+
+For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+**************************************************************************************************/
+
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<>();
+        int i=0;
+        int max = (int)Math.pow(2,n);
+        while(i<max){
+            list.add((i>>1)^i);
+            i++;
+        }
+        return list;
+    }
+}
